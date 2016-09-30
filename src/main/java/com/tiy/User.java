3b1a5c30658ddbc6fe159bdf0341051fffd4aef4
Jessica Tracy <j.tracy916@gmail.com>
2016-09-29 21:00:43 -0400

    User class

--- src/main/java/com/tiy/User.java
+++ src/main/java/com/tiy/User.java
@@ -1,7 +1,6 @@
 package com.tiy;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * Created by jessicatracy on 9/29/16.
@@ -9,4 +8,82 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue
+    int id;
+
+    @Column(nullable = false, unique = true)
+    String email;
+
+    @Column(nullable = false)
+    String password;
+
+    @Column(nullable = false)
+    String firstName;
+
+    @Column(nullable = false)
+    String lastName;
+
+    @Column
+    String techSkills;
+
+    public User() {
+    }
+
+    public User(String email, String password, String firstName, String lastName, String techSkills) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.techSkills = techSkills;
+    }
+
+    //Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTechSkills() {
+        return techSkills;
+    }
+
+    public void setTechSkills(String techSkills) {
+        this.techSkills = techSkills;
+    }
 }
