6db9567f8a91dc87b8bab287aafdb7e4ae5fbe56
Jessica Tracy <j.tracy916@gmail.com>
2016-09-29 21:25:52 -0400

    Friend class and UserEvent class

--- src/main/java/com/tiy/UserEvent.java
+++ src/main/java/com/tiy/UserEvent.java
@@ -1,7 +1,6 @@
 package com.tiy;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * Created by jessicatracy on 9/29/16.
@@ -9,4 +8,39 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "user_events")
 public class UserEvent {
+    @Id
+    @GeneratedValue
+    int id;
+
+    @ManyToOne
+    User user;
+
+    @ManyToOne
+    Event event;
+
+    
+    //Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
 }
